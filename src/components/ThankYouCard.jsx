18d0af8ad8882dca086923fba71634056564fec8
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2021-02-17 14:57:24 -0600

    updated info and message

--- src/components/ThankYouCard.jsx
+++ src/components/ThankYouCard.jsx
@@ -10,7 +10,7 @@ const thankYouCard = () => {
     <p className="card-text2">Thank you so much for taking the time to chat with me about the CSE position with Pantheon.  I really enjoyed learning about Pantheon and the great things you guys are doing.  I am very grateful to have had the chance to meet with you and would love the opportunity to learn and grow with Pantheon.  I certainly appreciate hearing abou the ins and outs of the position! It was so nice to meet you, and thank you agian for your time!</p>
     <p className="signature">Amanda Parrott</p>
     <p className="phone">205-587-4272</p>
-    <p className="email">amanda.m.parrott@gmail.com</p>
+    <p className="email">amanda.m.parrott@gmail.com </p>
   </div>
 </div>
     )
